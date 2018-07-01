d928053101a47f14d717424fcea82e359e90c9e9
Bruno Cavalcanti <bjcc@cin.ufpe.br>
2018-07-01 16:34:12 -0300

    adicionando comentarios

--- Agentes/defense_agent.py
+++ Agentes/defense_agent.py
@@ -7,15 +7,15 @@ import time
 # Funcoes de Defesa
 _NOOP = actions.FUNCTIONS.no_op.id
 _SELECT_POINT = actions.FUNCTIONS.select_point.id
-_TRAIN_DARK_TEMPLAR = actions.FUNCTIONS.Train_DarkTemplar_quick.id
-_TRAIN_STALKER = actions.FUNCTIONS.Train_Stalker_quick.id
-_RALLY_UNITS_MINIMAP = actions.FUNCTIONS.Rally_Units_minimap.id
-_SELECT_ARMY = actions.FUNCTIONS.select_army.id
-_ATTACK_MINIMAP = actions.FUNCTIONS.Attack_minimap.id
+_TRAIN_DARK_TEMPLAR = actions.FUNCTIONS.Train_DarkTemplar_quick.id #cria o soldado dark
+_TRAIN_STALKER = actions.FUNCTIONS.Train_Stalker_quick.id #cria o soldado stalker
+_RALLY_UNITS_MINIMAP = actions.FUNCTIONS.Rally_Units_minimap.id # <= Cria o ponto de encontro ao criar as unidades. Nao sei como podemos usar ja que n temos mapa certo definido
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id # <= Selecionar todas as minhas tropas (acho que n posso usar porque temos tropas divididas)
+_ATTACK_MINIMAP = actions.FUNCTIONS.Attack_minimap.id # <= Usar quando precisar me defender
 
 # Features
-_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
-_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index # <=
+_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index  # <= Util para selecionar unidades
 
 # Unidades ID Protoss
 _PROTOSS_STALKER = 74
@@ -37,6 +37,7 @@ class DefenseAgent(base_agent.BaseAgent):
 	stalkers_selected = False
 	dark_templars_selected = False
 
+	#Seleciona os Stalkers
 	def SelectStalkers(self, obs):
 		super(DefenseAgent, self).step(obs)
 
@@ -49,7 +50,7 @@ class DefenseAgent(base_agent.BaseAgent):
 
 		return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, stalkers])
 
-
+	#Seleciona os Dark Templars
 	def SelectDarkTemplars(self, obs):
 		super(DefenseAgent, self).step(obs)
 
@@ -62,10 +63,11 @@ class DefenseAgent(base_agent.BaseAgent):
 
 		return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, dark_templars])
 
+	#Criar os Stalkers
 	def TrainStalkers(self, obs):
 		super(DefenseAgent, self).step(obs)
 
-		if(!max_capacity 
+		if(not max_capacity 
 			and _TRAIN_STALKER in obs.observation["available_actions"]
 			and _STALKERS_MAX > 0):
 
@@ -76,10 +78,11 @@ class DefenseAgent(base_agent.BaseAgent):
 		return actions.FunctionCall(_NOOP, [])
 
 
+	#Cria os DarkTemplairs
 	def TrainDarkTemplairs(self, obs):
 		super(DefenseAgent, self).step(obs)
 
-		if(!max_capacity 
+		if(not max_capacity 
 			and _TRAIN_DARK_TEMPLAR in obs.observation["available_actions"]
 			and _DARK_TEMPLAR_MAX > 0):
 
